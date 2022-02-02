ee1b45fb830e2c9093a9b89ed9097a29057ea581
Antonio <antonio.garciamn@gmail.com>
2022-02-02 13:28:30 -0300

    feat: basic configs mongo

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-import express from "express";
+import express, { json } from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const serve = express();
+serve.use(cors());
+serve.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+	db = mongoClient.db("mywallet");
+});
 
 serve.listen(5000, () => console.log("ouvindo"));
