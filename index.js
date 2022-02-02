faf8315f934d425a8d078ce26ad24e3188331abf
Antonio <antonio.garciamn@gmail.com>
2022-02-02 17:38:30 -0300

    feat: router post sign-in

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express, { json } from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import { BSONRegExp, MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import joi from "joi";
+import { v4 as uuid } from "uuid";
 dotenv.config();
 
 const serve = express();
@@ -35,15 +36,49 @@ serve.post("/sign-up", async (req, res) => {
 	try {
 		const passwordHash = bcrypt.hashSync(user.password, 10);
 
-		const newUser = await db.collection("users").insertOne({
+		await db.collection("users").insertOne({
 			...user,
 			password: passwordHash,
 		});
 
-		res.send(201);
+		res.sendStatus(201);
 	} catch {
-		return res.sendStatus(401);
+		return res.sendStatus(422);
 	}
+});
+
+serve.post("/sign-in", async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await db.collection("users").findOne({ email });
+
+	if (user && bcrypt.compareSync(password, user.password)) {
+		const token = uuid();
+
+		await db.collection("sessions").insertOne({
+			userID: user._id,
+			token,
+		});
+
+		res.send(token);
+	} else return res.sendStatus(401);
+});
+
+serve.get("/user", async (req, res) => {
+	const { authorization } = req.headers;
+
+	const token = authorization?.replace("Bearer ", "");
+	if (!token) return res.sendStatus(401);
+
+	const session = await db.collection("sessions").findOne({ token });
+	if (!session) return res.sendStatus(401);
+
+	const user = await db.collection("users").findOne({ _id: session.userId });
+	if (!user) return res.sendStatus(401);
+
+	delete user.password;
+
+	res.send(user);
 });
 
 serve.listen(5000, () => console.log("ouvindo"));
