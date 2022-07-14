2a42b2205f28214fc396e4cbec4aa111ef925fdf
Antonio <antonio.garciamn@gmail.com>
2022-07-14 10:25:43 -0300

    make it better

--- src/db.js
+++ src/db.js
@@ -1,11 +1,18 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 await mongoClient.connect();
 
 const db = mongoClient.db("mywallet");
 
 export default db;
+
+
+
